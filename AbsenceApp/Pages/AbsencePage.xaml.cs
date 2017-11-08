b2283ba38013bbd814fcc89bc4fb879b2b67aeda
Peter Bork <pbn@intelligentbanker.com>
2017-11-08 22:02:18 +0100

    Adding simple dialog box with the information from the input fields

--- AbsenceApp/Pages/AbsencePage.xaml.cs
+++ AbsenceApp/Pages/AbsencePage.xaml.cs
@@ -40,5 +40,16 @@ namespace AbsenceApp.Pages
             //};
 
         }
+        void SubmitAbsence(object sender, System.EventArgs e) {
+            var FromDate = this.FromDate.Date.ToString();
+            var FromTime = this.FromTime.Time.ToString();
+
+            var ToDate = this.ToDate.Date.ToString();
+            var ToTime = this.ToTime.Time.ToString();
+
+            var Reason = this.Reason.Text;
+
+            DisplayAlert("Absence submitted!", "From: " + FromDate + " " + FromTime + ", To: " + ToDate + " " +  ToTime +". Reason: " + Reason + "!", "OK");
+        }
     }
 }
