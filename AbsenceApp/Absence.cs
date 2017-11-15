f875bf6024c8a449dc2edb85271a87be438378ea
Peter Bork <pbn@intelligentbanker.com>
2017-11-16 00:26:11 +0100

    * Adding very basic absence class * Adding details to the list on the history page + month picker * Tweaking some styling

---
+++ AbsenceApp/Absence.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace AbsenceApp {
+    public class Absence {
+        public string Date { get; set; }
+        public string MissingHours { get; set; }
+    }
+}
--- AbsenceApp/Pages/HistoryPage.xaml.cs
+++ AbsenceApp/Pages/HistoryPage.xaml.cs
@@ -1,39 +1,62 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
-namespace AbsenceApp.Pages
-{
-    public partial class HistoryPage : ContentPage
-    {
+namespace AbsenceApp.Pages {
+
+    public partial class HistoryPage : ContentPage {
+
+        ObservableCollection<Absence> AbsenceHistory = new ObservableCollection<Absence>();
+
         public HistoryPage() {
             InitializeComponent();
             Title = "History";
 
-            this.ListView.ItemsSource = new string[]{
-              "monovcxv",
-              "monodroid",
-              "monotouch",
-              "monorail",
-              "monodevelop",
-              "monotone",
-              "monopoly",
-              "monomodal",
-              "mononucleosis"
-            };
+            //listView.ItemsSource = new string[]{
+            //    "mono",
+            //    "monodroid",
+            //    "monotouch",
+            //    "monorail",
+            //    "monodevelop",
+            //    "monotone",
+            //    "monopoly",
+            //    "monomodal",
+            //    "mononucleosis"
+            //};
 
-            //    Content = new StackLayout
-            //    {
-            //        Children = {
-            //            new Label {
-            //                Text = "History",
-            //                HorizontalOptions = LayoutOptions.Center,
-            //                VerticalOptions = LayoutOptions.CenterAndExpand
+
+            DateTime startDate = new DateTime(2017, 1, 1);
+            DateTime currentDate = DateTime.Now;
+
+            string[] months = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            List<string> monthArray = new List<string>();
+
+            int i = 0;
+            while (startDate <= currentDate) {
+                monthArray.Add(months[startDate.Month - 1]+ " " + startDate.Year);
+                startDate = startDate.AddMonths(1);
+                i++;
+            }
+            MonthPicker.ItemsSource = monthArray;
+            MonthPicker.SelectedIndex = DateTime.Now.Month - 1;
+
+            AbsenceHistory.Add(new Absence() { Date = "16-10-2017", MissingHours = "2 hours missed" });
+            AbsenceHistory.Add(new Absence() { Date = "16-11-2017", MissingHours = "5 hours missed" });
+
+            AbsenceListView.ItemsSource = AbsenceHistory;
+
+            //        var data = new List<List<String>>{
+            //            new List<string>{
+            //                "test", "test2"
+            //            },
+            //            new List<string>{
+            //                "test", "test2"
             //            }
-            //        }
-            //    };
-            //}
+            //        };
+            //        listView.ItemsSource = data;
+            //    }
         }
     }
 }
