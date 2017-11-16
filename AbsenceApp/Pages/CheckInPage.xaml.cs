32b8a630a5b445c99b02e5a4df4ff855af37e1b0
Peter Bork <pbn@intelligentbanker.com>
2017-11-16 01:06:36 +0100

    Adding EAL location to the map with a pin + moving the maps starting point to that location aswell #cantsleep

--- AbsenceApp/Pages/CheckInPage.xaml.cs
+++ AbsenceApp/Pages/CheckInPage.xaml.cs
@@ -12,6 +12,18 @@ namespace AbsenceApp.Pages
         {
             InitializeComponent();
             Title = "Check-In";
+            var ealLocation = new Position(55.403458, 10.3771453); // Latitude, Longitude
+            var pin = new Pin {
+                Type = PinType.Place,
+                Position = ealLocation,
+                Label = "custom pin",
+                Address = "custom detail info"
+            };
+            MyMap.Pins.Add(pin);
+            MyMap.HasZoomEnabled = true;
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                ealLocation, Distance.FromMiles(0.3)));
+
             //var map = new Map(
             //MapSpan.FromCenterAndRadius(
             //        new Position(37, -122), Distance.FromMiles(0.3)))
