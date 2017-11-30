69380421914b405c8a63b5655840abe163cec89b
Peter Bork <pbn@intelligentbanker.com>
2017-11-30 08:34:17 +0100

    Adding models

---
+++ AbsenceApp/Models/Attendances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AbsenceApp.Models {
+    public class Attendances {
+        
+        public int id { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public DateTime started_at { get; set; }
+        public DateTime ended_at { get; set; }
+        public Boolean accepted { get; set; }
+        public int user_id { get; set; }
+
+        public Attendances() {}
+    }
+}
---
+++ AbsenceApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AbsenceApp.Models {
+    public class User {
+        public string name { get; set; }
+        public string password { get; set; }
+        public string api_token { get; set; }
+        public int group_id { get; set; }
+
+        public User(){}
+    }
+}
